268ea14a2368899ddf100dbd0737877c1fe87cb5
TsukasaCh <92731359+TsukasaCh@users.noreply.github.com>
2021-12-17 20:13:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/bash
 
-pkg update
-pkg upgrade
-apt-get install libwebp
-apt-get install mc
-apt-get install ffmpeg
-apt-get install wget
-apt-get install tesseract
-apt-get install nmap
-npm i pm2 -g
-pm2 npm install ffmpeg
+pkg update -y
+pkg upgrade -y
+pkg install git -y
+pkg install tty-clock -y
+pkg install sox -y
+pkg install jq -y
+pkg install figlet -y
+pkg install toilet -y
+pkg install wget -y
+pkg install imagemagick -y
+pkg install ffmpeg -y
+pkg install webp
+pkg install libwebp -y
+pkg install tesseract -y
+pkg install nmap
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm i pm2 -g
+pm2 install ffmpeg 
+ 
 
-echo "All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
+#!/usr/bin/bash
+
+tty-clock -s -S -x -c -C 2 -b -B
+clear
 pm2 start zeeone.js
-pm2 save && pm2 monit
+pm2 save
+pm2 monit
