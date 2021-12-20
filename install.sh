ea35429a3a93f22dbfdded7095068d86bc06e304
TsukasaCh <92731359+TsukasaCh@users.noreply.github.com>
2021-12-21 05:56:38 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,11 @@
 
 apt update -y
 apt upgrade -y
-apt install git -y
 apt install tty-clock -y
 apt install sox -y
 apt install jq -y
 apt install figlet -y
 apt install toilet -y
-apt install libwebp -y
 apt install tesseract -y
 apt install nmap
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
